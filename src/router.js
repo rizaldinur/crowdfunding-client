a428cebdc0ca23f41ffb02a7c194acbbe51f26f7
rizaldinur <rnur71@gmail.com>
2024-12-09 19:20:09 +0700

    initial routes definition

--- src/router.js
+++ src/router.js
@@ -2,6 +2,41 @@ import { Navigate, createBrowserRouter } from "react-router";
 import RootLayout from "./routes/layouts/RootLayout.jsx";
 import ErrorBoundary from "./routes/ErrorBoundary.jsx";
 
-const router = createBrowserRouter({});
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    errorElement: <ErrorBoundary />,
+    children: [
+      {
+        path: "/",
+        element: <HomeLayout />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "discover",
+            element: <DiscoverProjects />,
+          },
+          {
+            path: "project/details/:projectId",
+            element: <ProjectDetailsLayout />,
+          },
+          {
+            element: <ProtectedRoute />,
+            children: [
+              { path: "profile/:profileId" },
+              { path: "settings" },
+              { path: "start-project" },
+            ],
+          },
+        ],
+      },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <SignUp /> },
+    ],
+  },
+]);
 
 export default router;
