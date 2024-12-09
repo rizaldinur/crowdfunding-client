e549327ad01922b5b18fe6ba047c176a16a9cb3f
rizaldinur <rnur71@gmail.com>
2024-12-10 05:10:49 +0700

    Update ProtectedRoutes.jsx

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,9 +1,11 @@
 import { Navigate, Outlet } from "react-router";
 
-function ProtectedRoutes({ children }) {
+function ProtectedRoutes({ user, children }) {
   //get param profile id
   //get user context
   if (!user) return <Navigate to="/login" />;
 
   return children ? children : <Outlet />;
 }
+
+export default ProtectedRoutes;
