3be202a8ba485b1cf8f228d150a06920606dd7e3
rizaldinur <rnur71@gmail.com>
2024-12-10 19:01:30 +0700

    initial index page

---
+++ src/routes/Index.jsx
@@ -0,0 +1,13 @@
+import { Stack, Typography } from "@mui/material";
+
+function Index() {
+  return (
+    <Stack direction="row" height="100%">
+      <Stack alignItems="center">
+        <Typography>You are in INDEX PAGE</Typography>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default Index;
