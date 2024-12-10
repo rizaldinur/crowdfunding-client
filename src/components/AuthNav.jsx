c16266183269bb488dbf01287d9393b0c80669b3
rizaldinur <rnur71@gmail.com>
2024-12-10 19:01:22 +0700

    create auth nav component

---
+++ src/components/AuthNav.jsx
@@ -0,0 +1,21 @@
+import { Box, Link, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router";
+
+function AuthNav() {
+  return (
+    <Box
+      textAlign="center"
+      padding="10px 0"
+      sx={{ borderBottom: "solid 1px", borderBottomColor: "divider" }}
+      marginBottom={10}
+    >
+      <Link component={RouterLink} to="/" underline="none">
+        <Typography variant="h5" fontWeight={500} display="inline">
+          RUANG MODAL
+        </Typography>
+      </Link>
+    </Box>
+  );
+}
+
+export default AuthNav;
