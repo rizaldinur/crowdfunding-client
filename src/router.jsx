c40fb6dacdefd6227bf48eae883f1c04205aa9b6
rizaldinur <rnur71@gmail.com>
2024-12-10 19:02:39 +0700

    use jsx ext, adjustment for testing

---
+++ src/router.jsx
@@ -0,0 +1,96 @@
+import { Navigate, createBrowserRouter } from "react-router";
+import RootLayout from "./routes/layouts/RootLayout.jsx";
+import ErrorBoundary from "./routes/ErrorBoundary.jsx";
+import Login from "./routes/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    errorElement: (
+      <RootLayout>
+        <ErrorBoundary />
+      </RootLayout>
+    ),
+    children: [
+      // {
+      //   path: "/",
+      //   element: <MainLayout />,
+      //   children: [
+      //     {
+      //       index: true,
+      //       element: <Index />,
+      //     },
+      //     {
+      //       path: "discover",
+      //       element: <DiscoverProjects />,
+      //     },
+      //     {
+      //       path: "project/details/:projectId",
+      //       element: <ProjectDetailsLayout />,
+      //       children: [
+      //         { index: true, element: <Navigate to="campaign" /> },
+      //         { index: "campaign", element: <CampaignPage /> },
+      //         { path: "updates", element: <CampaignUpdatesPage /> },
+      //         { path: "comments", element: <CommentsPage /> },
+      //         { path: "faqs", element: <FaqsPage /> },
+      //       ],
+      //     },
+      //     {
+      //       path: "profile/:profileId",
+      //       element: <ProfileLayout user={currentUser} />,
+      //       children: [
+      //         { index: true, element: <AboutProfile /> },
+      //         { path: "backed", element: <BackedProjects /> },
+      //         { path: "saved", element: <SavedProjects /> },
+      //         { path: "projects", element: <CreatedProjects /> },
+      //       ],
+      //     },
+      //     {
+      //       element: <ProtectedRoute user={currentUser} />,
+      //       children: [
+      //         {
+      //           path: "settings",
+      //           element: <SettingsLayout />,
+      //           children: [
+      //             { index: true, element: <Navigate to="account" /> },
+      //             { path: "account", element: <AccountSettings /> },
+      //             { path: "profile", element: <ProfileSettings /> },
+      //           ],
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
+      // {
+      //   path: "/profile/:profileId/projects/:projectId",
+      //   element: <ProtectedRoute user={currentUser} />,
+      //   children: [
+      //     { path: "build-overview", element: <BuildOverview /> },
+      //     {
+      //       path: "edit",
+      //       element: <BuildProjectLayout />,
+      //       children: [
+      //         { index: true, element: <Navigate to="basic" /> },
+      //         { path: "basic", element: <BasicPage /> },
+      //         { path: "story", element: <StoryPage /> },
+      //         { path: "payment", element: <PaymentPage /> },
+      //         { path: "preview", element: <PreviewPage /> },
+      //       ],
+      //     },
+      //   ],
+      // },
+      { path: "/login", element: <Login /> },
+      // { path: "/signup", element: <SignUp /> },
+      // {
+      //   path: "/start-project",
+      //   element: (
+      //     <ProtectedRoute user={currentUser}>
+      //       <StartProject />
+      //     </ProtectedRoute>
+      //   ),
+      // },
+    ],
+  },
+]);
+
+export default router;
