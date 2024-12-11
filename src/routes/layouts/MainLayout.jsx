060d8a948ad940bdc5c1167329576a905ee4fe62
rizaldinur <rnur71@gmail.com>
2024-12-11 21:07:26 +0700

    minor change

--- src/routes/layouts/MainLayout.jsx
+++ src/routes/layouts/MainLayout.jsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router";
+import MainHeader from "../../components/MainHeader";
 
 function MainLayout() {
-  return <Outlet />;
+  return (
+    <>
+      <MainHeader />
+      <Outlet />
+    </>
+  );
 }
 export default MainLayout;
