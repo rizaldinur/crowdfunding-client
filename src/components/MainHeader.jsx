db7c197a92bd41bcbab1375b40591bc0507441e5
rizaldinur <rnur71@gmail.com>
2024-12-14 20:12:29 +0700

    basic side navbar UI

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -16,6 +16,9 @@ import {
   Drawer,
   FormControl,
   Link,
+  List,
+  ListItem,
+  ListItemButton,
   ListItemIcon,
   ListItemText,
   MenuList,
@@ -30,21 +33,14 @@ const pages = ["Mulai Proyek", "Jelajahi", "Login"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function MainHeader() {
-  const user = true;
+  const user = false;
   const navigate = useNavigate();
   const { currentTheme, handleThemeChange } = useContext(ThemeContext);
-  const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
+  const [openNav, setOpenNav] = useState(false);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
@@ -59,14 +55,79 @@ function MainHeader() {
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
+          <Drawer open={openNav} onClose={() => setOpenNav(false)}>
+            <Box sx={{ width: 250 }}>
+              <List>
+                <ListItem>
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      navigate("login");
+                    }}
+                  >
+                    <FormControl id="search__form-control">
+                      <OutlinedInput
+                        fullWidth
+                        id="search"
+                        aria-describedby="my-helper-text"
+                        name="search"
+                        placeholder="Search"
+                        color="inherit"
+                        size="small"
+                        startAdornment={
+                          <IconButton type="submit" color="inherit">
+                            <Search />
+                          </IconButton>
+                        }
+                        sx={{
+                          paddingLeft: 0,
+                          borderRadius: 2,
+                        }}
+                      />
+                    </FormControl>
+                  </form>
+                </ListItem>
+                {pages.map((page) => (
+                  <ListItem
+                    key={page}
+                    sx={{
+                      display: page === "Login" && user && "none",
+                    }}
+                  >
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <Settings />
+                      </ListItemIcon>
+                      <ListItemText primary={page} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+              {user && <Divider />}
+              {user && (
+                <List>
+                  {settings.map((setting) => (
+                    <ListItem key={setting}>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          <Settings />
+                        </ListItemIcon>
+                        <ListItemText primary={setting} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              )}
+            </Box>
+          </Drawer>
           <IconButton
             size="large"
             aria-label="account of current user"
             aria-controls="menu-appbar"
             aria-haspopup="true"
-            onClick={handleOpenNavMenu}
+            onClick={() => setOpenNav(true)}
             color="inherit"
-            sx={{ display: { xs: "initial", md: "none" } }}
+            sx={{ display: { xs: "inherit", md: "none" } }}
           >
             <MenuIcon />
           </IconButton>
@@ -75,6 +136,7 @@ function MainHeader() {
               RUANG MODAL
             </Typography>
           </Link>
+
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -83,9 +145,13 @@ function MainHeader() {
           >
             <FormControl
               id="search__form-control"
-              sx={{ display: { xs: "none", md: "block" } }}
+              sx={{
+                display: { xs: "none", md: "inherit" },
+                width: { xs: 300, lg: 400 },
+              }}
             >
               <OutlinedInput
+                fullWidth
                 id="search"
                 aria-describedby="my-helper-text"
                 name="search"
@@ -97,10 +163,14 @@ function MainHeader() {
                     <Search />
                   </IconButton>
                 }
-                sx={{ paddingLeft: 0, borderRadius: 2, width: 400 }}
+                sx={{
+                  paddingLeft: 0,
+                  borderRadius: 2,
+                }}
               />
             </FormControl>
           </form>
+
           <Box sx={{ display: { xs: "none", md: "flex" }, gap: 1 }}>
             <IconButton
               color="inherit"
@@ -114,9 +184,11 @@ function MainHeader() {
             {pages.map((page) => (
               <Button
                 key={page}
+                component={RouterLink}
+                to="login"
                 variant={page === "Mulai Proyek" ? "outlined" : "text"}
                 color="inherit"
-                sx={{ display: page === "Login" && user ? "none" : "initial" }}
+                sx={{ display: page === "Login" && user && "none" }}
               >
                 {page}
               </Button>
@@ -170,90 +242,6 @@ function MainHeader() {
               </MenuList>
             </Menu>
           </Box>
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
