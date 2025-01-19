63f9c8511585f1e16d131d9466685e5b751ee11c
rizaldinur <rnur71@gmail.com>
2025-01-19 22:31:27 +0700

    update featurecard

--- src/components/index/FeatureCard.jsx
+++ src/components/index/FeatureCard.jsx
@@ -1,15 +1,21 @@
 import {
+  Avatar,
   Box,
+  Button,
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   CardMedia,
   Chip,
   Link,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import { ThemeContext } from "../../routes/layouts/RootLayout";
 import { useContext } from "react";
+import { AccessTime, AccessTimeFilled, Person2 } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router";
 
 function FeatureCard() {
   const { currentTheme } = useContext(ThemeContext);
@@ -22,6 +28,7 @@ function FeatureCard() {
         display: "flex",
         flexDirection: { xs: "column", sm: "row" },
         width: 1,
+        gap: 1,
         position: "relative",
         transition: "0.2s",
         bgcolor: currentTheme === "dark" ? "background.paper" : "initial",
@@ -37,7 +44,8 @@ function FeatureCard() {
       <CardMedia
         sx={{
           borderRadius: 1,
-          width: { xs: 1, sm: 0.5 },
+          width: { xs: 1, sm: 0.6 },
+          height: { xs: 300, sm: "auto" },
           aspectRatio: "16/9",
         }}
         image="https://images2.alphacoders.com/247/247360.jpg"
@@ -48,31 +56,86 @@ function FeatureCard() {
         sx={{
           padding: 2,
           display: "flex",
-          gap: 2,
           width: { xs: "auto", sm: 0.5 },
           flexDirection: "column",
-          justifyContent: "space-between",
         }}
       >
-        <CardContent sx={{ padding: 0, width: 1 }}>
-          <Link
+        <Tooltip title="title here">
+          <Typography
+            variant="h4"
+            sx={{
+              color: "text.primary",
+              display: "-webkit-box", // Enables the use of line-clamp
+              WebkitBoxOrient: "vertical", // Required for multi-line truncation
+              overflow: "hidden", // Prevents overflow
+              WebkitLineClamp: 2, // Limits the text to 3 lines
+              mb: 1,
+              cursor: "pointer",
+            }}
+            component={Link}
             underline="hover"
-            color="textPrimary"
-            sx={{ cursor: "pointer" }}
+            href="#"
           >
-            <Typography noWrap gutterBottom variant="h4" component="div">
-              LizardLizardLizardLizardLizardLizardLizardLizardLizardLizardLizard
+            Kadal Gaming: Your Ultimate Gaming AI Companion Hardware
+          </Typography>
+        </Tooltip>
+        <CardHeader
+          sx={{ p: 0, mb: 4 }}
+          avatar={
+            <Avatar sx={{ width: 50, height: 50 }}>
+              <Person2 />
+            </Avatar>
+          }
+          title={
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component={Link}
+              underline="hover"
+              href="login"
+              sx={{
+                display: "-webkit-box", // Enables the use of line-clamp
+                WebkitBoxOrient: "vertical", // Required for multi-line truncation
+                overflow: "hidden", // Prevents overflow
+                WebkitLineClamp: 1, // Limits the text to 3 lines
+              }}
+            >
+              Team Xperience Kadal Gaming
             </Typography>
-          </Link>
+          }
+          subheader={
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                color: "text.primary",
+              }}
+            >
+              <AccessTimeFilled />
+              <Typography
+                variant="body1"
+                sx={{
+                  display: "-webkit-box", // Enables the use of line-clamp
+                  WebkitBoxOrient: "vertical", // Required for multi-line truncation
+                  overflow: "hidden", // Prevents overflow
+                  WebkitLineClamp: 1, // Limits the text to 3 lines
+                }}
+              >
+                Tersisa H hari • X% tercapai
+              </Typography>
+            </Box>
+          }
+        />
+        <CardContent sx={{ padding: 0, width: 1, mb: 10 }}>
           <Typography
             variant="body1"
             sx={{
-              color: "text.secondary",
+              color: "text.primary",
               display: "-webkit-box", // Enables the use of line-clamp
               WebkitBoxOrient: "vertical", // Required for multi-line truncation
               overflow: "hidden", // Prevents overflow
               WebkitLineClamp: 3, // Limits the text to 3 lines
-              lineHeight: 1.5, // Adjust this to match your design
+              // lineHeight: "normal", // Adjust this to match your design
             }}
           >
             Lizards are a widespread group of squamate reptiles, with over
@@ -105,7 +168,7 @@ function FeatureCard() {
             size="medium"
             variant="outlined"
             clickable
-            color="secondary"
+            color="inherit"
           />
           <Chip
             label="Share"
@@ -114,7 +177,7 @@ function FeatureCard() {
             size="medium"
             variant="outlined"
             clickable
-            color="secondary"
+            color="inherit"
           />
         </CardActions>
       </Box>
