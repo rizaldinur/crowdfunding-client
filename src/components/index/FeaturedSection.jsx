06d32490c9e862b939c4975de73dcb727a2b96d3
rizaldinur <rnur71@gmail.com>
2025-01-19 22:33:14 +0700

    move file

--- src/components/index/FeaturedSection.jsx
+++ src/components/index/FeaturedSection.jsx
@@ -1,5 +1,5 @@
 import { Box, Container, Typography } from "@mui/material";
-import FeatureCard from "./FeatureCard";
+import FeatureCard from "../card/FeatureCard";
 
 function FeaturedSection() {
   return (
