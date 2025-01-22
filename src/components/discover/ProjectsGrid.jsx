e23b0dd9e187d2cc9102fe43278c91e388670777
rizaldinur <rnur71@gmail.com>
2025-01-22 18:22:20 +0700

    change layout

--- src/components/discover/ProjectsGrid.jsx
+++ src/components/discover/ProjectsGrid.jsx
@@ -3,30 +3,38 @@ import MediaCard from "../card/MediaCard";
 
 function ProjectsGrid() {
   return (
-    <Box sx={{ borderBottom: "1px solid", borderBottomColor: "divider" }}>
+    <Box
+      sx={{
+        borderBottom: "1px solid",
+        borderBottomColor: "divider",
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Container maxWidth="xl">
-        <Box sx={{ py: 5 }}>
-          <Grid2 container spacing={4} sx={{ mb: 5 }}>
-            {Array(6)
-              .fill(0)
-              .map((val, i) => val + i + 1)
-              .map((item) => (
-                <Grid2
-                  size={{ xs: 12, sm: 6, md: 6, lg: 4 }}
-                  sx={{ display: "flex", justifyContent: "center" }}
-                >
-                  <MediaCard />
-                </Grid2>
-              ))}
-          </Grid2>
-          <Stack>
-            <Pagination
-              count={10}
-              color="secondary"
-              sx={{ placeSelf: "center" }}
-            />
-          </Stack>
-        </Box>
+        <Grid2 container spacing={4} sx={{ my: 5 }}>
+          {Array(6)
+            .fill(0)
+            .map((val, i) => val + i + 1)
+            .map((item) => (
+              <Grid2
+                size={{ xs: 12, sm: 6, md: 6, lg: 4 }}
+                sx={{ display: "flex", justifyContent: "center" }}
+              >
+                <MediaCard />
+              </Grid2>
+            ))}
+        </Grid2>
+      </Container>
+      <Container maxWidth="xl" sx={{ mt: "auto" }}>
+        <Stack sx={{ mb: 5 }}>
+          <Pagination
+            count={10}
+            color="secondary"
+            sx={{ placeSelf: "center" }}
+          />
+        </Stack>
       </Container>
     </Box>
   );
