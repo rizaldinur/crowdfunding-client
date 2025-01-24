0401f6fe572d95bfc73545e4ac5425560144d39d
rizaldinur <rnur71@gmail.com>
2025-01-24 18:53:33 +0700

    async options for locations, schools

--- src/components/start-project/StartProjectMain.jsx
+++ src/components/start-project/StartProjectMain.jsx
@@ -2,6 +2,7 @@ import {
   Autocomplete,
   Box,
   Button,
+  CircularProgress,
   Container,
   Stack,
   TextField,
@@ -10,11 +11,97 @@ import {
 import useThemeContext from "../../hooks/useThemeContext";
 import { UploadFile } from "@mui/icons-material";
 import VisuallyHiddenInput from "../input/VisuallyHiddenInput";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import debounce from "lodash.debounce";
+
+const fetchAllData = async () => {
+  const endpoint1 =
+    "https://api-sekolah-indonesia.vercel.app/sekolah/SMA?provinsi=050000&perPage=1488";
+  const endpoint2 =
+    "https://api-sekolah-indonesia.vercel.app/sekolah/SMK?provinsi=050000&perPage=1927";
+
+  const [data1, data2] = await Promise.all([
+    fetch(endpoint1).then((res) => res.json()),
+    fetch(endpoint2).then((res) => res.json()),
+  ]);
+
+  return [...data1.dataSekolah, ...data2.dataSekolah]; // Combine the data arrays
+};
 
 function StartProjectMain() {
   const { currentTheme } = useThemeContext();
   const [file, setFile] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [locationValue, setLocationValue] = useState(null);
+
+  const [inputValue, setInputValue] = useState("");
+  const [schools, setSchools] = useState([]);
+  const [schoolValue, setSchoolValue] = useState(null);
+  const [loadingSchool, setLoadingSchool] = useState(false);
+  const [onDebounce, setOnDebounce] = useState(false);
+
+  const handleLocationsOpen = () => {
+    setOpen(true);
+    (async () => {
+      if (locations.length > 0) {
+        return;
+      }
+      setLoading(true);
+      const response = await fetch(
+        "https://www.emsifa.com/api-wilayah-indonesia/api/regencies/35.json"
+      );
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+      setLocations([...data]);
+    })();
+  };
+
+  const handleLocationsClose = () => {
+    setOpen(false);
+    if (locations.length > 0) {
+      return;
+    }
+    setLocations([]);
+  };
+
+  // Debounce the fetch function
+  const debouncedFilter = debounce((query) => {
+    if (!query) {
+      setSchools([]);
+      return;
+    }
+
+    (async () => {
+      // console.log(query);
+
+      setLoadingSchool(true);
+      const data = await fetchAllData();
+
+      const filtered = data.filter((item) =>
+        item.sekolah.toLowerCase().includes(query.toLowerCase())
+      );
+      console.log(filtered);
+      setSchools(filtered);
+      setLoadingSchool(false);
+      setOnDebounce(false);
+    })();
+  }, 1000);
+
+  useEffect(() => {
+    if (inputValue) {
+      debouncedFilter(inputValue);
+    } else {
+      setSchools([]); // Clear options if input is empty
+    }
+
+    // Cleanup debounce on unmount
+    return () => {
+      debouncedFilter.cancel();
+    };
+  }, [inputValue]);
 
   return (
     <Container maxWidth="sm">
@@ -46,24 +133,49 @@ function StartProjectMain() {
           <Typography variant="caption" color="textSecondary">
             Tidak harus diisi sekarang. Kamu tetap bisa mengubahnya nanti.
           </Typography>
-          <Stack gap={2} sx={{ mt: 1 }}>
+          <Stack gap={2} sx={{ mt: 2 }}>
             <TextField variant="outlined" size="medium" label="Nama proyek" />
             <Autocomplete
-              id="tags-outlined"
+              id="categoryOptions"
               options={[]}
               getOptionLabel={(option) => option}
-              filterSelectedOptions
+              // filterSelectedOptions
               renderInput={(params) => (
                 <TextField {...params} label="Pilih kategori" />
               )}
             />
             <Autocomplete
-              id="tags-outlined"
-              options={[]}
-              getOptionLabel={(option) => option}
-              filterSelectedOptions
+              id="locationOptions"
+              options={locations}
+              open={open}
+              value={locationValue}
+              loading={loading}
+              autoComplete
+              onOpen={handleLocationsOpen}
+              onClose={handleLocationsClose}
+              onChange={(e, value) => {
+                setLocationValue(value);
+              }}
+              getOptionKey={(option) => option.id}
+              getOptionLabel={(option) => option.name}
               renderInput={(params) => (
-                <TextField {...params} label="Pilih lokasi" />
+                <TextField
+                  {...params}
+                  label="Pilih lokasi"
+                  slotProps={{
+                    input: {
+                      ...params.InputProps,
+                      endAdornment: (
+                        <>
+                          {loading ? (
+                            <CircularProgress color="inherit" size={20} />
+                          ) : null}
+                          {params.InputProps.endAdornment}
+                        </>
+                      ),
+                    },
+                  }}
+                />
               )}
             />
           </Stack>
@@ -85,16 +197,95 @@ function StartProjectMain() {
             <Typography variant="body1" fontWeight={700} color="textPrimary">
               Apa nama sekolahmu?*
             </Typography>
+            <Typography variant="caption" color="textSecondary">
+              Ketik sekolahmu dan tunggu sejenak. Jika tidak ada, pilih
+              "Tambahkan".
+            </Typography>
             <Autocomplete
-              id="tags-outlined"
-              options={[]}
-              getOptionLabel={(option) => option}
+              freeSolo
+              selectOnFocus
+              clearOnBlur
+              handleHomeEndKeys
+              id="school-options"
+              options={schools.map((school) => {
+                return { id: school.id, sekolah: school.sekolah };
+              })}
+              loading={loadingSchool}
+              value={schoolValue}
+              onClose={() => {
+                setLoadingSchool(false);
+                debouncedFilter.cancel();
+              }}
+              onChange={(event, newValue) => {
+                if (typeof newValue === "string") {
+                  setSchoolValue({
+                    id: "Others",
+                    sekolah: newValue.toUpperCase(),
+                  });
+                } else if (newValue && newValue.inputValue) {
+                  // Create a new value from the user input
+                  setSchoolValue({
+                    id: "Others",
+                    sekolah: newValue.inputValue.toUpperCase(),
+                  });
+                } else {
+                  setSchoolValue(newValue);
+                }
+                console.log(newValue);
+              }}
+              onInputChange={(event, newValue) => {
+                if (newValue.toLowerCase() !== inputValue.toLowerCase()) {
+                  setOnDebounce(true);
+                  setInputValue(newValue);
+                }
+              }}
+              getOptionLabel={(option) => {
+                if (typeof option === "string") {
+                  return option;
+                }
+
+                return option.sekolah;
+              }}
+              getOptionKey={(option) => option.id}
               filterSelectedOptions
-              form="skibidi"
+              filterOptions={(options, params) => {
+                const { inputValue } = params;
+                if (inputValue !== "" && options.length === 0 && !onDebounce) {
+                  options.push({
+                    inputValue,
+                    sekolah: `Tambahkan "${inputValue.toUpperCase()}"`,
+                  });
+                }
+                return options;
+              }}
+              renderOption={(props, option) => {
+                const { key, ...optionProps } = props;
+                return (
+                  <li key={key} {...optionProps}>
+                    {option.sekolah}
+                  </li>
+                );
+              }}
               renderInput={(params) => (
-                <TextField {...params} label="Nama sekolah" />
+                <TextField
+                  {...params}
+                  label="Nama sekolah"
+                  slotProps={{
+                    input: {
+                      ...params.InputProps,
+                      endAdornment: (
+                        <>
+                          {loadingSchool ? (
+                            <CircularProgress color="inherit" size={20} />
+                          ) : null}
+                          {params.InputProps.endAdornment}
+                        </>
+                      ),
+                    },
+                  }}
+                />
               )}
-              sx={{ mt: 1 }}
+              sx={{ mt: 2 }}
             />
             <Typography
               sx={{ mt: 3 }}
@@ -112,7 +303,7 @@ function StartProjectMain() {
             <Stack
               direction={{ xs: "column", sm: "row" }}
               gap={1}
-              sx={{ mt: 1, position: "relative" }}
+              sx={{ mt: 2, position: "relative" }}
             >
               <Button
                 component="label"
@@ -143,8 +334,8 @@ function StartProjectMain() {
               )}
             </Stack>
             <Typography variant="caption" color="textSecondary" sx={{ mt: 3 }}>
-              *Pastikan data sekolah sudah benar karena tidak bisa diubah lagi
-              setelah dikirim.
+              *Harus diisi. Pastikan data sekolah sudah benar karena tidak bisa
+              diubah lagi setelah dikirim.
             </Typography>
           </Stack>
         </Box>
