689b99032ebd2b7ab6168f515d1825ec54e88b70
rizaldinur <rnur71@gmail.com>
2025-01-28 18:51:40 +0700

    basic details tabs

--- src/routes/layouts/ProjectDetailsLayout.jsx
+++ src/routes/layouts/ProjectDetailsLayout.jsx
@@ -1,11 +1,13 @@
 import MainFooter from "../../components/navigation/MainFooter";
 import MainHeader from "../../components/navigation/MainHeader";
+import ProjectTabs from "../../components/project-details/ProejctTabs";
 import ProjectHead from "../../components/project-details/ProjectHead";
 
 function ProjectDetailsLayout() {
   return (
     <>
       <ProjectHead />
+      <ProjectTabs />
     </>
   );
 }
