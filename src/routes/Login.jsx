a6f23f45d11badba4cfce49eb3bbf5e73a4272e2
rizaldinur <rnur71@gmail.com>
2025-05-05 22:24:42 +0700

    login action and error handling

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,29 +1,91 @@
 import { Lock } from "@mui/icons-material";
 import {
+  Alert,
   Avatar,
   Box,
   Button,
+  CircularProgress,
   Container,
   Divider,
   Link,
+  Snackbar,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
-import { Link as RouterLink, useNavigate } from "react-router";
+import { Link as RouterLink, useFetcher, useNavigate } from "react-router";
 import { Form, useOutletContext } from "react-router";
 import AuthNav from "../components/navigation/AuthNav";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import CustomPasswordTextField from "../components/input/CustomPasswordTextField";
 
 function Login() {
   const [currentTheme] = useOutletContext();
   const navigate = useNavigate();
   document.title = "Masuk";
+  const [alertErrorOpen, setAlertErrorOpen] = useState(false);
+  const [alertSuccessOpen, setAlertSuccessOpen] = useState(false);
+
+  let fetcher = useFetcher();
+  let busy = fetcher.state !== "idle";
+  useEffect(() => {
+    if (fetcher.data) {
+      console.log(fetcher.data);
+      if (fetcher.data.error) {
+        return setAlertErrorOpen(true);
+      }
+      Cookies.set("jwt", fetcher.data.token, { expires: 15 / 1440 });
+      return setAlertSuccessOpen(true);
+    }
+  }, [fetcher.data]);
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    const name = e.target.name;
+    const value = e.target.value;
+    setForm((prevForm) => {
+      return { ...prevForm, [name]: value };
+    });
+  };
 
   return (
     <>
       <AuthNav />
+      <Snackbar
+        open={alertErrorOpen}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        onClose={() => setAlertErrorOpen(false)}
+      >
+        <Alert
+          sx={{
+            width: 300,
+          }}
+          onClose={() => setAlertErrorOpen(false)}
+          variant="filled"
+          severity="error"
+        >
+          {fetcher.data?.message || "Something went wrong."}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={alertSuccessOpen}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        onClose={() => {
+          setAlertSuccessOpen(false);
+          navigate("/");
+        }}
+        autoHideDuration={1000}
+      >
+        <Alert sx={{ width: 300 }} variant="filled" severity="success">
+          {fetcher.data?.message || "Sukses."}
+        </Alert>
+      </Snackbar>
       <Container maxWidth="sm">
         <Box
           sx={{
@@ -50,33 +112,42 @@ function Login() {
               Masuk
             </Typography>
             <Stack width={1}>
-              <TextField
-                type="email"
-                label="Email"
-                name="email"
-                sx={{ mb: 2 }}
-              />
-              <TextField
-                type="password"
-                label="Password"
-                name="password"
-                sx={{ mb: 2 }}
-              />
-              <Link href="#" underline="hover" marginBottom={3}>
-                <Typography variant="body2">Lupa Password?</Typography>
-              </Link>
-              <Button
-                variant="contained"
-                sx={{ width: 1, mb: 2 }}
-                onClick={() => {
-                  Cookies.set("token", "Base 64 token here", {
-                    expires: 1 / 96,
-                  });
-                  window.location.assign("/");
-                }}
-              >
-                Masuk
-              </Button>
+              <fetcher.Form method="post" noValidate>
+                <TextField
+                  type="email"
+                  label="Email"
+                  name="email"
+                  onChange={handleChange}
+                  error={alertErrorOpen}
+                  value={form.email}
+                  fullWidth
+                  sx={{ mb: 2 }}
+                />
+                <CustomPasswordTextField
+                  label="Password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  error={alertErrorOpen}
+                  value={form.password}
+                  fullWidth
+                  sx={{ mb: 2 }}
+                />
+                <Link href="#" underline="hover" marginBottom={3}>
+                  <Typography variant="body2">Lupa Password?</Typography>
+                </Link>
+                <Button
+                  variant="contained"
+                  disabled={busy}
+                  type="submit"
+                  sx={{ width: 1, mb: 2 }}
+                  startIcon={
+                    busy && <CircularProgress size={30} color="inherit" />
+                  }
+                >
+                  {busy ? "mengautentikasi" : "Masuk"}
+                </Button>
+              </fetcher.Form>
               <Divider component="div" role="presentation" sx={{ mb: 2 }}>
                 <Typography variant="subtitle2" color="divider">
                   Atau
@@ -95,5 +166,21 @@ function Login() {
     </>
   );
 }
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData);
+
+  const response = await fetch("http://localhost:8000/login", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  return data;
+};
 
 export default Login;
