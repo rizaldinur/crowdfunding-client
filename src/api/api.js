dd3141350556f0c999dfb431e326b71f9696f471
rizaldinur <rnur71@gmail.com>
2025-05-07 00:28:59 +0700

    create api handler for: login, signup, authjwt

---
+++ src/api/api.js
@@ -0,0 +1,35 @@
+export async function postSignup(postData) {
+  const response = await fetch("http://localhost:8000/signup", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+export async function postLogin(postData) {
+  const response = await fetch("http://localhost:8000/login", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+export const authenticateJWT = async (token) => {
+  const response = await fetch("http://localhost:8000/authenticate", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+
+  const data = await response.json();
+  return data;
+};
