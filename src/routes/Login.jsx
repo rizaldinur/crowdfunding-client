1e220928abb85a74dbac48931e4a5ac885ff1540
rizaldinur <rnur71@gmail.com>
2025-05-07 00:31:42 +0700

    improve login form validation, create action and loader for login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -13,30 +13,47 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { Link as RouterLink, useFetcher, useNavigate } from "react-router";
+import {
+  Await,
+  Navigate,
+  Link as RouterLink,
+  useFetcher,
+  useLoaderData,
+  useNavigate,
+} from "react-router";
 import { Form, useOutletContext } from "react-router";
 import AuthNav from "../components/navigation/AuthNav";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import CustomPasswordTextField from "../components/input/CustomPasswordTextField";
+import { getError } from "../utils/utils";
+import LoadingPage from "../components/LoadingPage";
+import validator from "validator";
+import { postLogin, authenticateJWT } from "../api/api";
 
 function Login() {
+  useEffect(() => {
+    document.title = "Masuk";
+  }, []);
+
   const [currentTheme] = useOutletContext();
   const navigate = useNavigate();
-  document.title = "Masuk";
   const [alertErrorOpen, setAlertErrorOpen] = useState(false);
   const [alertSuccessOpen, setAlertSuccessOpen] = useState(false);
+  const [formErrorData, setFormErrorData] = useState([]);
+  const { authData } = useLoaderData();
 
   let fetcher = useFetcher();
   let busy = fetcher.state !== "idle";
+
   useEffect(() => {
     if (fetcher.data) {
-      console.log(fetcher.data);
       if (fetcher.data.error) {
-        return setAlertErrorOpen(true);
+        setAlertErrorOpen(true);
+        return;
       }
-      Cookies.set("jwt", fetcher.data.token, { expires: 15 / 1440 });
-      return setAlertSuccessOpen(true);
+      Cookies.set("jwt", fetcher.data.data.token, { expires: 15 / 1440 });
+      setAlertSuccessOpen(true);
     }
   }, [fetcher.data]);
 
@@ -49,137 +66,223 @@ function Login() {
     console.log(e.target.value);
     const name = e.target.name;
     const value = e.target.value;
+
     setForm((prevForm) => {
       return { ...prevForm, [name]: value };
     });
+    //reset error on change
+    setFormErrorData((prevData) => {
+      return prevData.filter((error) => {
+        return error.body !== name;
+      });
+    });
+  };
+
+  const validateSubmit = (e) => {
+    let error = [];
+    let { email, password } = form;
+
+    email = validator.trim(email);
+    if (email.length > 0) {
+      email = validator.normalizeEmail(email);
+    }
+    if (email.length === 0) {
+      error.push({
+        body: "email",
+        message: "Harus diisi.",
+      });
+    }
+    if (!validator.isEmail(email)) {
+      error.push({
+        body: "email",
+        message: "Masukkan email yang benar.",
+      });
+    }
+    if (password.length === 0) {
+      error.push({
+        body: "password",
+        message: "Harus diisi.",
+      });
+    }
+    console.log(error);
+
+    if (error.length > 0) {
+      e.preventDefault();
+      setFormErrorData([...error]);
+      return;
+    }
+    setForm((prevForm) => {
+      return { ...prevForm, email: email, password: password };
+    });
+    setFormErrorData([...error]);
   };
 
   return (
-    <>
-      <AuthNav />
-      <Snackbar
-        open={alertErrorOpen}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        onClose={() => setAlertErrorOpen(false)}
-      >
-        <Alert
-          sx={{
-            width: 300,
-          }}
-          onClose={() => setAlertErrorOpen(false)}
-          variant="filled"
-          severity="error"
-        >
-          {fetcher.data?.message || "Something went wrong."}
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        open={alertSuccessOpen}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        onClose={() => {
-          setAlertSuccessOpen(false);
-          navigate("/");
-        }}
-        autoHideDuration={1000}
-      >
-        <Alert sx={{ width: 300 }} variant="filled" severity="success">
-          {fetcher.data?.message || "Sukses."}
-        </Alert>
-      </Snackbar>
-      <Container maxWidth="sm">
-        <Box
-          sx={{
-            mt: 10,
-            padding: 5,
-            placeSelf: "center",
-            width: 450,
-            boxSizing: "border-box",
-            maxWidth: 1,
-            border: "solid 1px",
-            borderColor: "divider",
-            borderRadius: 2,
-            bgcolor:
-              currentTheme === "light"
-                ? "background.default"
-                : "background.paper",
-          }}
-        >
-          <Stack alignItems="center">
-            <Avatar sx={{ bgcolor: "secondary.main", mb: 1 }}>
-              <Lock />
-            </Avatar>
-            <Typography variant="h4" color="textPrimary" marginBottom={3}>
-              Masuk
-            </Typography>
-            <Stack width={1}>
-              <fetcher.Form method="post" noValidate>
-                <TextField
-                  type="email"
-                  label="Email"
-                  name="email"
-                  onChange={handleChange}
-                  error={alertErrorOpen}
-                  value={form.email}
-                  fullWidth
-                  sx={{ mb: 2 }}
-                />
-                <CustomPasswordTextField
-                  label="Password"
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  error={alertErrorOpen}
-                  value={form.password}
-                  fullWidth
-                  sx={{ mb: 2 }}
-                />
-                <Link href="#" underline="hover" marginBottom={3}>
-                  <Typography variant="body2">Lupa Password?</Typography>
-                </Link>
-                <Button
-                  variant="contained"
-                  disabled={busy}
-                  type="submit"
-                  sx={{ width: 1, mb: 2 }}
-                  startIcon={
-                    busy && <CircularProgress size={30} color="inherit" />
-                  }
+    <Suspense fallback={<LoadingPage />}>
+      <Await resolve={authData}>
+        {(authData) => {
+          if (authData.data.authenticated) {
+            return <Navigate to=".." replace />;
+          }
+          return (
+            <>
+              <AuthNav />
+              <Snackbar
+                open={alertErrorOpen}
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                onClose={() => setAlertErrorOpen(false)}
+              >
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  onClose={() => setAlertErrorOpen(false)}
+                  variant="filled"
+                  severity="error"
                 >
-                  {busy ? "mengautentikasi" : "Masuk"}
-                </Button>
-              </fetcher.Form>
-              <Divider component="div" role="presentation" sx={{ mb: 2 }}>
-                <Typography variant="subtitle2" color="divider">
-                  Atau
-                </Typography>
-              </Divider>
-              <Typography variant="body2" color="textSecondary">
-                Belum punya akun?&nbsp;
-                <Link component={RouterLink} to="/signup" underline="hover">
-                  Daftar
-                </Link>
-              </Typography>
-            </Stack>
-          </Stack>
-        </Box>
-      </Container>
-    </>
+                  {fetcher.data?.message || "Something went wrong."}
+                </Alert>
+              </Snackbar>
+              <Snackbar
+                open={alertSuccessOpen}
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+                onClose={() => {
+                  setAlertSuccessOpen(false);
+                  navigate("/", { replace: true });
+                }}
+                autoHideDuration={1000}
+              >
+                <Alert sx={{ width: 300 }} variant="filled" severity="success">
+                  {fetcher.data?.message || "Sukses."}
+                </Alert>
+              </Snackbar>
+              <Container maxWidth="sm">
+                <Box
+                  sx={{
+                    mt: 10,
+                    padding: 5,
+                    placeSelf: "center",
+                    width: 450,
+                    boxSizing: "border-box",
+                    maxWidth: 1,
+                    border: "solid 1px",
+                    borderColor: "divider",
+                    borderRadius: 2,
+                    bgcolor:
+                      currentTheme === "light"
+                        ? "background.default"
+                        : "background.paper",
+                  }}
+                >
+                  <Stack alignItems="center">
+                    <Avatar sx={{ bgcolor: "secondary.main", mb: 1 }}>
+                      <Lock />
+                    </Avatar>
+                    <Typography
+                      variant="h4"
+                      color="textPrimary"
+                      marginBottom={3}
+                    >
+                      Masuk
+                    </Typography>
+                    <Stack width={1}>
+                      <fetcher.Form
+                        method="post"
+                        onSubmit={validateSubmit}
+                        noValidate
+                      >
+                        <TextField
+                          type="email"
+                          label="Email"
+                          name="email"
+                          onChange={handleChange}
+                          error={
+                            Boolean(getError("email", formErrorData)) ||
+                            alertErrorOpen
+                          }
+                          helperText={getError("email", formErrorData)?.message}
+                          value={form.email}
+                          fullWidth
+                          sx={{ mb: 2 }}
+                        />
+                        <CustomPasswordTextField
+                          label="Password"
+                          type="password"
+                          name="password"
+                          onChange={handleChange}
+                          error={
+                            Boolean(getError("password", formErrorData)) ||
+                            alertErrorOpen
+                          }
+                          helperText={
+                            getError("password", formErrorData)?.message
+                          }
+                          value={form.password}
+                          fullWidth
+                          sx={{ mb: 2 }}
+                        />
+                        <Link href="#" underline="hover" marginBottom={3}>
+                          <Typography variant="body2">
+                            Lupa Password?
+                          </Typography>
+                        </Link>
+                        <Button
+                          variant="contained"
+                          disabled={busy}
+                          type="submit"
+                          sx={{ width: 1, mb: 2 }}
+                          startIcon={
+                            busy && (
+                              <CircularProgress size={30} color="inherit" />
+                            )
+                          }
+                        >
+                          {busy ? "mengautentikasi" : "Masuk"}
+                        </Button>
+                      </fetcher.Form>
+                      <Divider
+                        component="div"
+                        role="presentation"
+                        sx={{ mb: 2 }}
+                      >
+                        <Typography variant="subtitle2" color="divider">
+                          Atau
+                        </Typography>
+                      </Divider>
+                      <Typography variant="body2" color="textSecondary">
+                        Belum punya akun?&nbsp;
+                        <Link
+                          component={RouterLink}
+                          to="/signup"
+                          underline="hover"
+                        >
+                          Daftar
+                        </Link>
+                      </Typography>
+                    </Stack>
+                  </Stack>
+                </Box>
+              </Container>
+            </>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
 }
 
-export const action = async ({ request }) => {
+export const loginLoader = () => {
+  const authData = authenticateJWT(Cookies.get("jwt") || "");
+  return { authData };
+};
+
+export const loginAction = async ({ request }) => {
+  console.log(request);
+
   const formData = await request.formData();
   const postData = Object.fromEntries(formData);
 
-  const response = await fetch("http://localhost:8000/login", {
-    method: "POST",
-    body: JSON.stringify(postData),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  const data = await response.json();
+  const data = await postLogin(postData);
   return data;
 };
 
