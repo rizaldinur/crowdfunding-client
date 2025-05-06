a5b02050f41fd0b7da90c30ec4d061acdf7a6904
rizaldinur <rnur71@gmail.com>
2025-05-06 23:52:41 +0700

    create loadingpage for whole page loading on suspense fallback

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,29 @@
+import { CircularProgress, Stack, Typography } from "@mui/material";
+import ThemeContainer from "./ThemeContainer";
+import { useMemo, useState } from "react";
+import themes from "../styles/themes";
+
+function LoadingPage() {
+  const [currentTheme, setCurrentTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  const activeTheme = useMemo(() => themes[currentTheme], [currentTheme]);
+
+  return (
+    <ThemeContainer
+      activeTheme={activeTheme}
+      sx={{ alignItems: "center", justifyContent: "center" }}
+    >
+      <Stack sx={{ alignItems: "center" }} gap={2}>
+        <CircularProgress />
+        <Typography variant="h5" color="textPrimary">
+          {" "}
+          Sedang memuat halaman...
+        </Typography>
+      </Stack>
+    </ThemeContainer>
+  );
+}
+
+export default LoadingPage;
