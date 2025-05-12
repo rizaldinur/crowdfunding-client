dc5cccef251e8cace936b2967ab7debc8fc19f68
rizaldinur <rnur71@gmail.com>
2025-05-12 20:37:29 +0700

    change to checklist icon

--- src/components/progress/CircularProgressWithLabel.jsx
+++ src/components/progress/CircularProgressWithLabel.jsx
@@ -3,31 +3,41 @@ import PropTypes from "prop-types";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { Avatar } from "@mui/material";
+import { Check } from "@mui/icons-material";
 
 function CircularProgressWithLabel(props) {
   return (
     <Box sx={{ position: "relative", display: "inline-flex" }}>
-      <CircularProgress variant="determinate" {...props} />
-      <Box
-        sx={{
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          position: "absolute",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Typography
-          variant="caption"
-          component="div"
-          sx={{ color: "text.secondary" }}
-        >
-          {`${Math.round(props.value)}%`}
-        </Typography>
-      </Box>
+      {props.value === 100 ? (
+        <Avatar sx={{ bgcolor: "primary.main" }}>
+          <Check />
+        </Avatar>
+      ) : (
+        <>
+          <CircularProgress variant="determinate" {...props} />
+          <Box
+            sx={{
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              position: "absolute",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="caption"
+              component="div"
+              sx={{ color: "text.secondary" }}
+            >
+              {`${Math.round(props.value)}%`}
+            </Typography>
+          </Box>
+        </>
+      )}
     </Box>
   );
 }
