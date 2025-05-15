f42f7989057b71370959b171b0813e6985aa1b9d
rizaldinur <rnur71@gmail.com>
2025-05-15 15:22:23 +0700

    add api on buildpage loader and postbuild action

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
+import { getToken } from "../utils/utils";
+
 export async function postSignup(postData) {
   const response = await fetch("http://localhost:8000/signup", {
     method: "POST",
@@ -22,9 +24,43 @@ export async function postLogin(postData) {
   return data;
 }
 
+export async function postBuildBasicForm(postData, pathname) {
+  let baseurl = "http://localhost:8000";
+  let url = baseurl + pathname;
+
+  let token = getToken();
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  return data;
+}
+
 export const authenticateJWT = async (token) => {
   const response = await fetch("http://localhost:8000/authenticate", {
     method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+export const getFilledForm = async (path) => {
+  let baseurl = "http://localhost:8000";
+  let url = baseurl + path;
+  let token = getToken();
+
+  const response = await fetch(url, {
+    method: "get",
     headers: {
       Authorization: "Bearer " + token,
     },
