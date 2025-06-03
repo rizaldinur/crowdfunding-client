07e27fc53685c51ec00ace756f89ccf0a1e60470
rizaldinur <rnur71@gmail.com>
2025-06-03 22:36:28 +0700

    fix layout shift on hover , scrolling behavior, forwardRef, and scroll button

--- src/components/index/RecommendedSection.jsx
+++ src/components/index/RecommendedSection.jsx
@@ -1,8 +1,56 @@
-import { Typography } from "@mui/material";
-import { Box, Container } from "@mui/system";
+import { IconButton, Typography } from "@mui/material";
+import { Box, Container, Grid, width } from "@mui/system";
 import MediaCard from "../card/MediaCard";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import { useEffect, useRef, useState } from "react";
 
 function RecommendedSection({ data = [] }) {
+  const boxContainerRef = useRef(null);
+  const boxRef = useRef(null);
+  const [boxScrollWidth, setBoxScrollWidth] = useState(0); // State to store the scrollWidth
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(false);
+
+  useEffect(() => {
+    const checkScrollPosition = () => {
+      if (boxContainerRef.current) {
+        setCanScrollLeft(boxContainerRef.current.scrollLeft > 0); // Update state based on current scrollLeft
+        setCanScrollRight(
+          boxContainerRef.current.scrollLeft <
+            boxContainerRef.current.scrollWidth -
+              boxContainerRef.current.clientWidth
+        );
+      }
+    };
+
+    // Initial check
+    checkScrollPosition();
+
+    // Optional: Update width on window resize
+    const handleResize = () => {
+      if (boxRef.current) {
+        setBoxScrollWidth(boxRef.current.scrollWidth);
+      }
+    };
+
+    //initial setWidth
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    const container = boxContainerRef.current;
+    if (container) {
+      container.addEventListener("scroll", checkScrollPosition);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (container) {
+        container.removeEventListener("scroll", checkScrollPosition);
+      }
+    };
+  });
+
   return (
     <Box
       id="recommended"
@@ -14,24 +62,90 @@ function RecommendedSection({ data = [] }) {
     >
       <Container maxWidth="lg">
         <Box sx={{ paddingTop: 5, paddingBottom: 5 }}>
-          <Typography
-            sx={{ position: "relative", mb: 3 }}
-            variant="h6"
-            color="textSecondary"
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
           >
-            REKOMENDASI
-          </Typography>
-          <Box sx={{ display: "flex", gap: 3 }}>
-            {data.length > 0 ? (
-              data.map((project, index) => {
-                return <MediaCard key={index} data={project} />;
-              })
-            ) : (
-              <Typography color="textSecondary" sx={{ mx: "auto" }}>
-                Tidak ada data.
-              </Typography>
-            )}
+            <Typography
+              sx={{ position: "relative", mb: 3 }}
+              variant="h6"
+              color="textSecondary"
+            >
+              REKOMENDASI
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 1,
+                alignItems: "center",
+                color: "text.primary",
+              }}
+            >
+              <IconButton
+                size="small"
+                sx={{ border: "1px solid" }}
+                disabled={!canScrollLeft}
+                onClick={() => {
+                  boxContainerRef.current.scrollLeft -= boxScrollWidth;
+                }}
+              >
+                <ChevronLeft />
+              </IconButton>
+              <IconButton
+                size="small"
+                sx={{ border: "1px solid" }}
+                disabled={!canScrollRight}
+                onClick={() => {
+                  console.log(boxRef.current);
+                  console.log(boxContainerRef.current);
+                  boxContainerRef.current.scrollLeft += boxScrollWidth;
+                }}
+              >
+                <ChevronRight />
+              </IconButton>
+            </Box>
           </Box>
+          {data.length > 0 ? (
+            <Box
+              ref={boxContainerRef}
+              sx={{
+                display: "flex",
+                gap: 3,
+                overflowX: "auto",
+                msOverflowStyle: "none",
+                scrollbarWidth: "none",
+                whiteSpace: "nowrap",
+                scrollSnapType: "x mandatory",
+                scrollBehavior: "smooth",
+                ":hover": {
+                  p: 2,
+                  m: -2,
+                },
+              }}
+            >
+              {data.map((project, i) => {
+                return (
+                  <MediaCard
+                    ref={i === 0 ? boxRef : null}
+                    key={`featured-${i}`}
+                    data={project}
+                    sx={{
+                      flexShrink: 0,
+                      scrollMarginLeft: 2,
+                      scrollSnapAlign: "start",
+                    }}
+                  />
+                );
+              })}
+            </Box>
+          ) : (
+            <Typography color="textSecondary" sx={{ mx: "auto" }}>
+              Tidak ada data.
+            </Typography>
+          )}
         </Box>
       </Container>
     </Box>
